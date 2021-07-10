ca17657be10d2f35d93607edcfc10dfa800a17cc
Davidcpu <daviddurandz67@gmail.com>
2021-07-10 13:11:06 -0600

    Update NumeroFlotante.c
    Conclucion, pero aun hay un erro. pero sin errores de sintaxis

--- NumeroFlotante.c
+++ NumeroFlotante.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main( )
 {
     //Tipos de dato Estados
-    typedef enum TEstado{q0,q1,q2,q3,q4};
-
+    // enum TEstado{q0,q1,q2,q3,q4};
+    typedef enum {q0,q1,q2,q3,q4} TEstado;
     //variable de estado
     TEstado Estado;
 
@@ -26,11 +27,76 @@ int main( )
 
     printf("Ingrese una cadena de numeros: ");
     scanf("%s", &cadena);
+    longitud = strlen(cadena);
+    printf("tamanio cadena ingresada: %d", longitud);
 
-    longitud = strlen( cadena );
+    longitud = strlen(cadena);
 
+    while(longitud>i)
+    {
+        Simbolo = cadena[i];
+        switch ( Estado)
+        {
+        case q0:
+            //Si es un simbolo valido cambia de estado en este caso menos(-) o mas(+)
+            if(Simbolo == '-' || Simbolo == '+')
+            {
+                Estado = q1;
+            }
+            break;
+        case q1:
+            //Si simbolo es un digito entre cero y nueve cambia de estado
+            if(Simbolo >= '0' && Simbolo <= '9')
+            {
+                Estado = q2;
+            }
+            break;
+        case q2:
+            //Si el simbolo es de nuevo un digito  entre cero y nueve no cambia de estado
+            if(Simbolo >= '0' && Simbolo <= '9')
+            {
+                Estado = q2;
+            }
+            else
+            {
+                //Caso contrario si simbolo es un punto cambia de estado
+                if(Simbolo == '.')
+                {
+                    Estado = q3;
+                }
+            }
+            break;
+        case q3:
+            //Si simbolo es un digito entre cero y nueve cambia de estado
+            if(Simbolo >= '0' && Simbolo <= '9')
+            {
+                Estado = q4;
+            }
+            break;
+        case q4:
+            //Si simbolo es un digito entre cero y nueve no cambia de estado
+            if(Simbolo >= '0' && Simbolo <= '9')
+            {
+                Estado = q4;
+            }
+            break;
+        }
+        //si se leyo algun caracter no valido
+        if(!(Simbolo >= '0' && Simbolo <= '9')&&
+                !(Simbolo >= '0' && Simbolo <= '9')&&
+                !(Simbolo == '.'))
+        {
+            break;
+        }
+        //iterador aunmeta y avanza al siguiente caracter de la cadena
+        i++;
 
+    }
 
+    if(Estado == q4)
+        printf("Cadena acepatada");
+    else
+        printf("Cadena No acepatada");
 
     return 0;
 }
