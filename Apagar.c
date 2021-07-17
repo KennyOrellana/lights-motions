75ea8721149b71d6ec1eda15441f1e29ed6cbabf
Davidcpu <daviddurandz67@gmail.com>
2021-07-16 21:48:32 -0600

    Create Apagar.c
    Comenzando con la funcion apagar

---
+++ Apagar.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char TablaSimbolos[7] = {'mientras','si','sino','para','definir','encender','apagar'};
+char Temporal[200];
+char cadena[200];
+
+//Contador
+int contador;
+
+bool si();
+bool palabra( char cad[100]);
+bool sino();
+
+
+int main()
+{
+
+    return 0;
+
+}
+
+
+
+bool apagar()
+{
+
+
+
+
+}
