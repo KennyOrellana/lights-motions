00a7ac3c5ffb80636f3b6b3f489cf848652fdae2
Davidcpu <daviddurandz67@gmail.com>
2021-07-18 13:28:12 -0600

    Update Apagar.c
    Siguiendo con la funcion apagar

--- Apagar.c
+++ Apagar.c
@@ -6,69 +6,101 @@
 
 
 char TablaSimbolos[7] = {'mientras','si','sino','para','definir','encender','apagar'};
-char Temporal[200];
-char cadena[200];
-char variable[200];
+char* Temporal[200];
+
+char* variable[200];
 
 //Contador
 int contador;
 
-bool si();
-bool palabra( char cad[100]);
-bool sino();
-bool cadena();
 
+
+bool cadena(char palb[]);
+bool apagar();
 
 int main()
 {
 
+
+
+
+    return 0;
+}
+
+
+bool apagar(){
     int centinela = 0;
     int i= 0;
     contador ++;
 
-    if(cadena(Temporal[contador])){
-        char variable = Temporal[contador];
+    if(cadena(Temporal[contador]))
+    {
+        char var = Temporal[contador];
         int arrayTamanio = sizeof variable/sizeof variable[0];
-        for(i = 0; i;< arrayTamanio; i++){
+        for(i = 0; i < arrayTamanio; i++)
+        {
 
-            if (variable.compare(Variables[i])==0){
-				if (cadenas(Temporal[contador])){
-					centinela = 1;
-            printf("Instruccion valida, apagando...%s", Temporal[contador]);
-            contador ++;
-            /*if(Temporal[contador] =="si" || Temporal[contador] == "sino" || Temporal[contador] == "mientras"
-                    || Temporal[contador] == "definir" || Temporal[contador] == "encender" || Temporal[contador] == "apagar")
-                    EncontrarComandos();
-						return true;
-					}else{
-						return false;
-					}
-                */
+            if (strcmp(var,variable[i])==0)
+            {
+                if (cadena(Temporal[contador]))
+                {
+                    centinela = 1;
+                    printf("Instruccion valida, apagando...%s", Temporal[contador]);
+                    contador ++;
+                 /*   if(Temporal[contador] =="si" || Temporal[contador] == "sino" || Temporal[contador] == "mientras"
+                            || Temporal[contador] == "definir" || Temporal[contador] == "encender" || Temporal[contador] == "apagar")
+                    {
+                        EncontrarComandos();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }*/
+
+                }
+                else
+                {
+                    printf("Error, instruccion no valida" );
+                    contador ++;
+                    return false;
+
+                }
+            }
+            else
+            {
+                printf("");
+            }
         }
-    }else {
-
-
-    printf("Error, instruccion no valido" );
-    contador ++;
-    //return false;
-    return 0;
+        if (centinela == 0)
+        {
+            printf("Comando  no valido, no existe intrucciion %s",Temporal[contador], " en memoria");
+        }
+        contador++;
     }
-    }else{
-				printf("");
-			}
-		}
-		if (centinela == 0){
-			printf("Comando  no valido, no existe intrucciion %s",Temporal[contador], " en memoria");
-		}
-		contador++;
-	}else{
-		printf("Error de sintaxis, instruccion si, no se reconocio como valido");
-		contador++;
-		//return false;
-	}
+    else
+    {
+        printf("Error de sintaxis, instruccion si, no se reconocio como valido");
+        contador++;
+        return false;
+    }
+    }
 
-    return 0;
 
+
+
+bool cadena(char palb[]){
+	int conta =0;
+	while(conta< strlen(palb)){
+        //Comprobamos que la palabra tenga los caracteres o números validos correspondientes
+        if(!(palb[conta]>='A' && palb[conta] <= 'Z' || palb[conta]>='a' && palb[conta] <= 'z'
+                        || palb[conta]>= '0' && palb[conta] <= '9'|| palb[conta]>='[' && palb[conta] <= '_' )){
+            return false;
+        }
+        conta++;
+    }
+    return true;
 }
+
 
 
