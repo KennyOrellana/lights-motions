a8bf7857afff10cb79e862ec829596582c95134f
Davidcpu <daviddurandz67@gmail.com>
2021-07-20 17:48:50 -0600

    Update NumeroFlotante.c
    Arreglado, finalizado

--- NumeroFlotante.c
+++ NumeroFlotante.c
@@ -15,7 +15,7 @@ int main( )
     //variables
     char secundari[100]; // es donde se almacenara la cadena
     int Simbolo;    //puede tomar el valor de un caracter de la tablas ASCII
-    bool EsNumero;
+
 
     int i; //iterador
     int longitud; // tomara el valor de longitud de la cadena
@@ -24,7 +24,8 @@ int main( )
     i = 0;
     longitud = 0;
     Estado = q0;
-  //  char cadena[0];
+    printf("estado %d: ", q0);
+
 
     printf("Ingrese una cadena de numeros: ");
     scanf("%s", &secundari);
@@ -32,24 +33,20 @@ int main( )
     longitud = strlen(secundari);
     printf("tamanio cadena ingresada: %d", longitud);
 
-  //  cadena[longitud] = secundari;
 
-  //  printf("letra: %s\n\n",cadena);
-
-   // longitud = strlen(cadena);
 
     while(secundari[i] !='\0')
     {
 
-        //Simbolo = cadena[i];
-        Simbolo = secundari[i];
-        //printf("simbolo: %s\n\n",secundari[i]);
+
+        //Simbolo = secundari[i];
+        //   EsNumero = (simbolo >='0'&& simbolo <= '9');
 
         switch ( Estado)
         {
         case q0:
             //Si es un simbolo valido cambia de estado en este caso menos(-) o mas(+)
-            if(Simbolo == '-' || Simbolo == '+' )
+            if(secundari[i] == '-' || secundari[i] == '+' )
             {
                 Estado = q1;
             }else{
@@ -57,49 +54,49 @@ int main( )
             }
             break;
         case q1:
-            //Si simbolo es un digito entre cero y nueve cambia de estado
-            if(Simbolo >= '0' && Simbolo <= '9')
+            //Si secundari[i] es un digito entre cero y nueve cambia de estado
+            if(secundari[i] >= '0' && secundari[i] <= '9')
             {
                 Estado = q2;
             }
             break;
         case q2:
             //Si el simbolo es de nuevo un digito  entre cero y nueve no cambia de estado
-            if(Simbolo >= '0' && Simbolo <= '9')
+            if(secundari[i] >= '0' && secundari[i] <= '9')
             {
                 Estado = q2;
             }
             else
             {
-                //Caso contrario si simbolo es un punto cambia de estado
-                if(Simbolo == '.')
+                //Caso contrario si secundari[i] es un punto cambia de estado
+                if(secundari[i] == '.')
                 {
                     Estado = q3;
                 }
             }
             break;
         case q3:
-            //Si simbolo es un digito entre cero y nueve cambia de estado
-            if(Simbolo >= '0' && Simbolo <= '9')
+            //Si secundari[i] es un digito entre cero y nueve cambia de estado
+            if(secundari[i] >= '0' && secundari[i] <= '9')
             {
                 Estado = q4;
             }
             break;
         case q4:
-            //Si simbolo es un digito entre cero y nueve no cambia de estado
-            if(Simbolo >= '0' && Simbolo <= '9')
+            //Si secundari[i] es un digito entre cero y nueve no cambia de estado
+            if(secundari[i] >= '0' && secundari[i] <= '9')
             {
                 Estado = q4;
             }
             break;
         }
-        //si se leyo algun caracter no valido
-        if(!(Simbolo >= '0' && Simbolo <= '9')&&
-                !(Simbolo >= '0' && Simbolo <= '9')&&
-                !(Simbolo == '.'))
+       //si se leyo algun caracter no valido
+     /*   if(!(secundari[i] >= '0' && secundari[i] <= '9') ||
+                !(secundari[i] >= '0' && secundari[i] <= '9')||
+                !(secundari[i] == '.'))
         {
             break;
-        }
+        }*/
         //iterador aunmeta y avanza al siguiente caracter de la cadena
         i++;
 
