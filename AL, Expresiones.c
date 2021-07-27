57ce4b3bf5e33362c305d2e3548d4486e412fc73
Daniel-Abraham266586 <71414922+Daniel-Abraham266586@users.noreply.github.com>
2021-07-26 19:11:10 -0500

    Update AL, Expresiones.c
    Expresiones v2

--- AL, Expresiones.c
+++ AL, Expresiones.c
@@ -16,14 +16,16 @@ bool asignacion(char cadena[]);
 
 bool mientras(char cadena[]);
 
+
 /*
 int main()
 {
 
     // Prueba Asignaciones
-    printf("%i\n", asignacion("A = 1"));
-    printf("%i\n", asignacion("AB2 = 22"));
+    printf("%i\n", asignacion("A = 1 - 1 + 1 * 2"));
+    printf("%i\n", asignacion("AA = 0"));
     printf("%i\n", asignacion("ABC = CDA"));
+
 
     // Prueba Operaciones
     printf("%i\n", operacion("1 + 1"));
@@ -61,6 +63,7 @@ int main()
     return 0;
 }
 */
+
 
 int esOperadorUnario(char c1, char c2)
 {
@@ -256,7 +259,7 @@ bool expresion(char cadena[])
                     else
                     {
                         Estado = qe;
-                        printf("Error q1: ");
+                        //printf("Error q1: ");
                     }
                     break;
                 }
@@ -468,7 +471,7 @@ bool asignacion(char cadena[])
                     else
                     {
                         Estado = qe;
-                        printf("Error q1: ");
+                        //printf("Error q1: ");
                     }
                     break;
                 }
@@ -495,7 +498,7 @@ bool asignacion(char cadena[])
                     else
                     {
                         Estado = qe;
-                        printf("Error q2: ");
+                        //printf("Error q2: ");
                     }
                     break;
                 }
@@ -517,160 +520,3 @@ bool asignacion(char cadena[])
         return false;
     }
 }
-
-
-/*
-bool mientras()
-{
-	contador++;
-	//Analisamos si el caracter despues del si, sea un parentesis abierto
-	if (Temporal[contador] == "(")
-    {
-		contador++;
-		//Revisamos que luego del parentesis lo que se ingresa sea un caracter o cadena
-		if (palabra(Temporal[contador]))
-        {
-			contador++;
-			//Analisamos que luego del caracter o cadena, sea un signo matematico valido
-			if (Temporal[contador]=="==" || Temporal[contador] == "!=" || Temporal[contador] == "<" || Temporal[contador] == ">")
-			{
-				contador++;
-				//revisamos que lo siguiente sea un numero
-				if (numero(Temporal[contador]))
-				{
-					contador++;
-					//Ahora revisamos si el siguiente caracter sea un parentesis, para cerrar el mientras
-					if (Temporal[contador]==")")
-					{
-						contador++;
-						//confirmamos que se abran los corchetes de la instruccion si
-						if (Temporal[contador]=="{")
-						{
-							contador++;
-							//confirmamos que se cierren los corchetes para darle fin a la instruccion
-							if (Temporal[contador]=="}")
-							{
-								cout<<"--Instruccion mientras, aceptada como valida\n";
-								contador++;
-								//hacemos la recursividad para que se analizen las siguientes instrucciones
-								if (Temporal[contador] == "si" || Temporal[contador] == "sino" || Temporal[contador] == "mientras" || Temporal[contador] == "definir" || Temporal[contador] == "encender" || Temporal[contador] == "apagar")
-								{
-									EncontrarComandos();
-									return true;
-								}
-                                else
-                                {
-									return false;
-								}
-							//todos los else, imprimen un mensaje de que las instrucciones no escribieron de la manera correcta
-							}
-							else
-                            {
-								cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-								contador++;
-								return false;
-							}
-						}
-						else
-                        {
-							cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-							contador++;
-							return false;
-						}
-					}
-				}
-				else
-                {
-					cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-					contador++;
-					return false;
-				}
-			}
-			else
-            {
-				cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-				contador++;
-				return false;
-			}
-		}
-		//Usamos el else if, para aceptar numeros tambien, luego del parentesis
-		else if(numero(Temporal[contador]))
-        {
-			contador++;
-			if (Temporal[contador]=="==" || Temporal[contador] == "!=" || Temporal[contador] == "<" || Temporal[contador] == ">")
-			{
-				contador++;
-				//revisamos que lo siguiente es una palabra
-				if (palabra(Temporal[contador]))
-				{
-					contador++;
-					//Ahora revisamos si el siguiente caracter sea un parentesis, para cerrar el si
-					if (Temporal[contador]==")")
-					{
-						contador++;
-						//confirmamos que se abran los corchetes de la instruccion si
-						if (Temporal[contador]=="{")
-						{
-							contador++;
-							//confirmamos que se cierren los corchetes para darle fin a la instruccion
-							if (Temporal[contador]=="}")
-							{
-								cout<<"--Instruccion mientras, aceptada como valida\";
-								contador++;
-								//hacemos la recursividad para que se analizen las siguientes instrucciones
-								if (Temporal[contador] == "si" || Temporal[contador] == "sino" || Temporal[contador] == "mientras" || Temporal[contador] == "definir" || Temporal[contador] == "encender" || Temporal[contador] == "apagar")
-								{
-									EncontrarComandos();
-									return true;
-								}
-                                else
-                                {
-									return false;
-								}
-							//todos los else, imprimen un mensaje de que las instrucciones no escribieron de la manera correcta
-							}
-							else
-                            {
-								cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-								contador++;
-								return false;
-							}
-						}
-						else
-                        {
-							cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-							contador++;
-							return false;
-						}
-					}
-				}
-				else
-				{
-					cout<<"--Error de sintaxis en la instruccion mientas, no se reconocio como valido\n";
-					contador++;
-					return false;
-				}
-			}
-			else
-			{
-				cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-				contador++;
-				return false;
-			}
-		}
-		else
-        {
-			cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-			contador++;
-			return false;
-		}
-	}
-	else
-	{
-		cout<<"--Error de sintaxis en la instruccion mientras, no se reconocio como valido\n";
-		contador++;
-		return false;
-	}
-}
-
-*/
