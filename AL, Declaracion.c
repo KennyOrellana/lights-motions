2f791a3920555ac25935d493a4af27c1d1b90168
Daniel-Abraham266586 <71414922+Daniel-Abraham266586@users.noreply.github.com>
2021-07-26 19:11:21 -0500

    Update AL, Declaracion.c
    declaraciones sin func

--- AL, Declaracion.c
+++ AL, Declaracion.c
@@ -14,7 +14,9 @@ bool declaracionFuncion();
 int main()
 {
     printf("%i\n", declaracion("BYTE AA = 0 + 1 + 2 + 3"));
-    printf("%i\n", declaracion("BYTE AA,BB,CC"));
+    printf("%i\n", declaracion("BYTE AA, BB, CC,"));
+
+    declaracionFuncion("ENTERO A, BYTE B, BOOL C");
     //printf("%i", strlen(TablaSimbolosTipos[1]));
     return 0;
 }
@@ -43,7 +45,7 @@ bool declaracion(char cadena[])
                 inicioTipo = i;
                 tipo = j;
 
-                strncpy(scTipo, &cadena[i], strlen(TablaSimbolosTipos[j]));
+                strncpy(scTipo, &cadena[i], strlen(TablaSimbolosTipos[j])+1);
                 scTipo[strlen(TablaSimbolosTipos[j])] = '\0';
                 //printf("%s : %i\n", scTipo, strlen(scTipo));
                 break;
@@ -54,6 +56,13 @@ bool declaracion(char cadena[])
         {
             break;
         }
+    }
+    tipoEncontrado = false;
+
+    //printf("%s", scTipo);
+    if(strcmp(scTipo, TablaSimbolosTipos[tipo]) == 0)
+    {
+        tipoEncontrado = true;
     }
 
     if(tipoEncontrado==true)
@@ -128,4 +137,110 @@ bool declaracion(char cadena[])
 
 bool declaracionFuncion(char cadena[])
 {
+    typedef enum {q0,q1,q2,q3,qe} TEstado;
+
+    TEstado Estado = q0;
+
+    int longitud = strlen(cadena);
+
+        char cadenaTemporal[longitud + 1];
+        strcpy(cadenaTemporal, cadena);
+        cadenaTemporal[longitud] = '\0';
+        //printf("%s : %i\n", subCadena_1, strlen(subCadena_1));
+
+        char *token;
+        token = strtok(cadenaTemporal, ",");
+
+        while(token != NULL)
+        {
+            printf("\n TOKEN: %s\n", token);
+            char partes[strlen(token)];
+            //memcpy (partes, token, strlen(token));
+            //partes[strlen(token)] = '\0';
+            strcpy(partes, token);
+
+            char *declaraciones;
+            //declaraciones = strtok(partes," ");
+            /*
+            int porciones = 0;
+
+            while(declaraciones != NULL)
+            {
+                printf("\n%s\n", declaraciones);
+
+                switch(Estado)
+                {
+                    case q0:
+                    {
+                        char scTipo[16];
+                        bool tipoEncontrado = false;
+                        int inicioTipo = 0;
+                        int tipo = 0;
+                        for(int i=0; i<longitud; i++)
+                        {
+                            for(int j=0; j<strlen(TablaSimbolosTipos); j++)
+                            {
+                                if(declaraciones[i] == TablaSimbolosTipos[j][0])
+                                {
+                                    tipoEncontrado = true;
+                                    inicioTipo = i;
+                                    tipo = j;
+
+                                    strncpy(scTipo, &cadena[i], strlen(TablaSimbolosTipos[j]));
+                                    scTipo[strlen(TablaSimbolosTipos[j])] = '\0';
+                                    //printf("%s : %i\n", scTipo, strlen(scTipo));
+                                    break;
+                                }
+                            }
+
+                            if(tipoEncontrado)
+                            {
+                                break;
+                            }
+                        }
+
+                        if(strcmp(scTipo, TablaSimbolosTipos[tipo]) == 0)
+                        {
+                            Estado = q1;
+                        }
+                        else
+                        {
+                            Estado = qe;
+                            printf("Error q0: \n");
+                        }
+
+                        break;
+                    }
+                    case q1:
+                    {
+                        if(variable(declaraciones))
+                        {
+                            Estado = q2;
+                        }
+                        else
+                        {
+                            Estado = qe;
+                            printf("Error q1: ");
+                        }
+
+                        break;
+                    }
+                    case q2:
+                    {
+                        if(porciones > 2)
+                        {
+                            printf("Error");
+                        }
+
+                        break;
+                    }
+                }
+
+                porciones++;
+                declaraciones = strtok(NULL, " ");
+            }
+            */
+
+            token = strtok(NULL, ",");
+        }
 }
