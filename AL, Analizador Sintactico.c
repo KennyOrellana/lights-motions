8541c8d622fcb574a536303445250f5769fc8fcc
Daniel-Abraham266586 <71414922+Daniel-Abraham266586@users.noreply.github.com>
2021-08-06 00:55:32 -0500

    Update AL, Analizador Sintactico.c
    Ingresar comprobar instrucciones

--- AL, Analizador Sintactico.c
+++ AL, Analizador Sintactico.c
@@ -1,5 +1,19 @@
+#include "AL, Mientras.c"
+
 #include<stdio.h>
 #include<stdlib.h>
+
+
+bool leerArchivo();
+void contarCantidadLineas();
+int cantidadLineas = 0;
+bool instrucciones(char cadena[]);
+
+typedef enum {q0, q1, q2, qf, qe} TEstado;
+TEstado Estado;
+
+int ambitosAbiertos = 0;
+int ambitosCerrados = 0;
 
 struct arrChar
 {
@@ -8,6 +22,22 @@ struct arrChar
 
 int main()
 {
+    if(leerArchivo())
+    {
+        printf("Analisis sin Errores\n");
+    }
+    else
+    {
+        printf("Se encontraron Errores\n");
+    }
+
+    return 0;
+}
+
+bool leerArchivo()
+{
+    TEstado Estado = q0;
+
     // Archivo donde esta el codigo
     char inputFilename[] = "entrada.txt";
     // Aqui se guardaran los errores y lecturas
@@ -17,23 +47,120 @@ int main()
     *aux.string = "";
 
     int lineaActual=0;
-    char *devf;
+    char *linea;
 
     FILE *ifp;
     ifp = fopen(inputFilename, "r");
 
-    for(int i=0; devf!=NULL; i++)
+    for(int i=0; linea!=NULL; i++)
     {
         lineaActual = i;
-        devf = fgets(aux.string, 4000, ifp);
+        linea = fgets(aux.string, 4000, ifp);
 
-        /// Aqui adentro haremos las comprobaciones linea a linea si hay errores o no.
-        printf("%s", devf);
+        switch(Estado)
+        {
+            case q0:
+            {
+                if(strcmp(linea, "INICIO\n") == 0)
+                {
+                    Estado = q1;
+                }
+                else
+                {
+                    Estado = qe;
+                    printf("Error q0");
+                }
+                break;
+            }
+            case q1:
+            {
+                if(lineaActual == cantidadLineas)
+                {
+                    if(strcmp(linea, "FINAL\n") == 0)
+                    {
+                        Estado = q2;
+                    }
+                    else
+                    {
+                        Estado = qe;
+                        printf("Error q1");
+                    }
+                }
+                else if(instrucciones(linea))
+                {
+                    Estado = q1;
+                }
+                else
+                {
+                    Estado = qe;
+                    printf("Error q1");
+                }
+                break;
+            }
+        }
 
+        if(Estado == qe)
+        {
+            break;
+        }
+
+        printf("%s", linea);
+    }
+
+    if(Estado == q2)
+    {
+        if(ambitosAbiertos == ambitosCerrados)
+        {
+            Estado = qf;
+        }
+        else
+        {
+            Estado = qe;
+            printf("Error q1");
+        }
+    }
+
+    if(Estado == qf)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 
     printf("\n\nEl archivo tiene %d lineas\n", lineaActual);
     fclose (ifp);
+}
 
-    return 0;
+void contarCantidadLineas()
+{
+    char inputFilename[] = "entrada.txt";
+
+    struct arrChar aux;
+    *aux.string = "";
+
+    int lineaActual=0;
+    char *linea;
+
+    FILE *ifp;
+    ifp = fopen(inputFilename, "r");
+
+    for(int i=0; linea!=NULL; i++)
+    {
+        lineaActual = i;
+        linea = fgets(aux.string, 4000, ifp);
+    }
+
+    cantidadLineas = lineaActual;
+}
+
+bool instrucciones(char cadena[])
+{
+    int longitud = strlen(cadena);
+
+    /// Aqui comprobar que las lineas sean: Declaraciones, Ciclos, Condicionales, Expresiones, Funciones
+
+    // Si es Funcion, Cico o Decicion, aumentar el contador de abertura ambitos
+    // Si es la palanbra Fin aumentar el contador de cierre de ambitos
 }
